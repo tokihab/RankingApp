5d4f61bb67e8c8b9af7ab93ffd7fd77494cc71b8
toni ihab <t609kii@gmail.com>
2025-09-12 13:34:31 +0300

    added a home page and changed background color

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,8 +1,13 @@
 import RankItemsContainer from "./components/RankItemsContainer";
 import MovieImageArr from "./components/MovieImages.js";
 import AlbumImageArr from "./components/AlbumImages.js";
+import Home from "./components/Home.js";
 
 const AppRoutes = [
+    {
+        index: true,
+        element: <Home />
+    },
     {
         path: '/rank-movies',
         element: <RankItemsContainer dataType={1} imgArr={MovieImageArr} />
---
+++ ClientApp/src/components/Home.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Introduction from './Introduction';
+
+const Home = () => {
+    return (
+        <div>
+            <Introduction />
+        </div>
+    );
+};
+
+export default Home;
