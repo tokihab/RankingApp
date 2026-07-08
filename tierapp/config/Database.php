7a8e049bf31f2d39b136c82cdbc01755eb929868
toni ihab <t609kii@gmail.com>
2026-07-08 03:43:49 +0300

    db port missing

--- tierapp/config/Database.php
+++ tierapp/config/Database.php
@@ -10,13 +10,11 @@ class Database {
 
     public function getConnection() {
         $this->conn = null;
-        
-        // 3. Assign the environment variable INSIDE the function
         $this->password = getenv("AIVEN_DB_PASS"); 
 
         try {
-            // 4. Connect using $this->password
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->username, $this->password);
+            
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
